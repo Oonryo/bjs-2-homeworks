9bfad57f567d2318d9b07dee18398f461d21fcf6
Oonryo <oonryo@gmail.com>
2023-04-21 15:32:00 +0300

    Done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(el => el.gender === gender).reduce((accum, averageAge, index, arrayLength) => accum + averageAge.age / arrayLength.length, 0);
+	return result
 }
